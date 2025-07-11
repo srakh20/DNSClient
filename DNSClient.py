36699afceec830d5c392c7a09e1c5e38ae93969d
Sarah R <sarah.rakhamimov@baruchmail.cuny.edu>
2025-07-11 17:40:48 -0400

    DNS client update

--- DNSClient.py
+++ DNSClient.py
@@ -12,7 +12,7 @@ domainList  = ['example.com.','safebank.com.','google.com.','nyu.edu.','legitsit
 # Define a function to query the local DNS server for the IP address of a given domain name
 def query_local_dns_server(domain,question_type):
     resolver = dns.resolver.Resolver()
-    resolver.nameservers = [local_host_ip]
+    resolver.nameservers = [socket.gethostbyname(socket.gethostname())]
     answers = resolver.resolve(domain, question_type) # provide the domain and question_type
 
     ip_address = answers[0].to_text()
