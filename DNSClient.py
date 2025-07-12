0c60e4e6c3dd7f1f7786a3f2c6518eea24d29913
Sarah R <sarah.rakhamimov@baruchmail.cuny.edu>
2025-07-12 01:15:40 -0400

    changed local ip to 127.0.0.1

--- DNSClient.py
+++ DNSClient.py
@@ -2,7 +2,7 @@ import dns.resolver
 import socket
 
 # Set the IP address of the local DNS server and a public DNS server
-local_host_ip = "192.168.1.255"
+local_host_ip = "127.0.0.1"
 real_name_server = "8.8.8.8" # Research public DNS servers to find a valid DNS server IP address to use
 
 
